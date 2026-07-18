63e5a476ba156fa3e5e6e4d070c6904f5dc3c3eb
moroxixi <mohamadrofialwani321@gmail.com>
2026-07-18 20:40:39 +0700

    auto: 2026-07-18 20:40

--- Pencatatan-Buku-Kas/config.js
+++ Pencatatan-Buku-Kas/config.js
@@ -1,4 +1,4 @@
 const ENDPOINT_URL = "https://script.google.com/macros/s/AKfycbyfiAuzhfrDhyRkqGVPTXXd3xQINVpoJNPm97BJnt-ioaGqchtqGOLR84g9Km5Q5U-6/exec";
 const SCRIPT_URL = ENDPOINT_URL;
 
-const STOK_SCRIPT_URL = "GANTI_DENGAN_URL_WEB_APP_SETORAN_TEMPURA_WONTON";
+const STOK_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzbFc2R3o9GIwEZgPKpJFc4UJvDimRI0iskhydzMcWh_0TKFwt815Z8JTI55I4Mo9w/exec";
