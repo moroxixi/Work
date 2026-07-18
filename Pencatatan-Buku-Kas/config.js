0d0488d4b10853538c4a0d771f2dbc1b501d4a24
moroxixi <mohamadrofialwani321@gmail.com>
2026-07-18 15:33:19 +0700

    auto: 2026-07-18 15:33

--- Pencatatan-Buku-Kas/config.js
+++ Pencatatan-Buku-Kas/config.js
@@ -1,1 +1,2 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyfiAuzhfrDhyRkqGVPTXXd3xQINVpoJNPm97BJnt-ioaGqchtqGOLR84g9Km5Q5U-6/exec";
+const ENDPOINT_URL = "https://script.google.com/macros/s/AKfycbyfiAuzhfrDhyRkqGVPTXXd3xQINVpoJNPm97BJnt-ioaGqchtqGOLR84g9Km5Q5U-6/exec";
+const SCRIPT_URL = ENDPOINT_URL;
