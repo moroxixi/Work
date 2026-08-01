a4436fcddceffef34bf97bee7ec53594ad54c052
moroxixi <mohamadrofialwani321@gmail.com>
2026-08-02 00:10:28 +0700

    auto: 2026-08-02 00:10

--- Pencatatan-Buku-Kas/config.js
+++ Pencatatan-Buku-Kas/config.js
@@ -1,4 +1,4 @@
-const ENDPOINT_URL = "https://script.google.com/macros/s/AKfycbyJEGS37SBwahjHTBIxq5n6Dw3ioqqcMN065n01bWxwG2EnPAZbq05K4_wH-qg0ZNPC/exec";
+const ENDPOINT_URL = "https://script.google.com/macros/s/AKfycbwYNScW5c4xQNO-oG_oQy6pQUEYjPPThD9wUcsbDnqfuDh74zMuIAEjbu0Iuiy4QtkV/exec";
 const SCRIPT_URL = ENDPOINT_URL;
 
 const STOK_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzbFc2R3o9GIwEZgPKpJFc4UJvDimRI0iskhydzMcWh_0TKFwt815Z8JTI55I4Mo9w/exec";
--- Wonton/Config/config.js
+++ Wonton/Config/config.js
@@ -2,4 +2,4 @@
 // karena keduanya memanggil GAS project yang sama (Code.gs / report.gs).
 // Ganti URL di baris ini SAJA setelah redeploy GAS — tidak perlu edit
 // Tempura/index.html atau Wonton/index.html.
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxErhJKLybcwvTNc24N6h-Y-67MxqPPFFG6NjoePQwsXukF1qaDscCElkCq_DeuyIM/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwlCtC5KUR5tMk29p7hnR0Q7X5XGp4AeHMJV-bhJpUnjyJCmBRgxjTxFJdC-isxkHyj/exec";
