8cb71c82618495ecdda72f35659f9ed76d891bcd
moroxixi <mohamadrofialwani321@gmail.com>
2026-08-12 22:31:12 +0700

    auto: 2026-08-12 22:31

--- Wonton/Config/config.js
+++ Wonton/Config/config.js
@@ -2,4 +2,4 @@
 // karena keduanya memanggil GAS project yang sama (Code.gs / report.gs).
 // Ganti URL di baris ini SAJA setelah redeploy GAS — tidak perlu edit
 // Tempura/index.html atau Wonton/index.html.
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwMP8zVY1bL3ALzCHbp84WnPQv-K989FAUzPZfI3GOo7vSQhPfbJU5lV0ANFWUt-KlP/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyNMJ5RUTnn0e_xomBTt03Jrdn1BJEt3sM2eHWclgsqIfo84_vPeabGxi11eEjJh-Kf/exec";
