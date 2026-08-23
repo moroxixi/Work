3adea366500b232e4c01401a6b450b812efb2f37
moroxixi <mohamadrofialwani321@gmail.com>
2026-08-23 19:39:15 +0700

    auto: 2026-08-23 19:39

--- Pencatatan-Buku-Kas/config.js
+++ Pencatatan-Buku-Kas/config.js
@@ -1,4 +1,6 @@
+// Buku Kas
 const ENDPOINT_URL = "https://script.google.com/macros/s/AKfycbwYNScW5c4xQNO-oG_oQy6pQUEYjPPThD9wUcsbDnqfuDh74zMuIAEjbu0Iuiy4QtkV/exec";
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxZJcGq59aOJFfKwvcKGNqLPNmfW6zBXecfS6EmhkZGwXlCRicZvl9V6pyzAu4UwKjp/exec";
-
+// Scan Struk
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw0Aq9jEkaDe3gfDOOIKBEuzSYgWjMIRas4UKz_-o3X4gql116oigIz3bNQpi8miqiB/exec";
+// Report
 const STOK_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzbFc2R3o9GIwEZgPKpJFc4UJvDimRI0iskhydzMcWh_0TKFwt815Z8JTI55I4Mo9w/exec";
