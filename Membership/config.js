8e9413880318e7350d6a12e92879c9909f2c4db8
moroxixi <mohamadrofialwani321@gmail.com>
2026-09-17 12:56:45 +0700

    auto: 2026-09-17 12:56

--- Membership/config.js
+++ Membership/config.js
@@ -9,5 +9,5 @@
  * setelah deploy (format: https://script.google.com/macros/s/.../exec).
  */
 const MAO_CONFIG = {
-  GAS_WEB_APP_URL: "<GAS_WEB_APP_URL_DI_SINI>"
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxpujqY0krSmEz7QoaetDJuL105ObmNTjRdnpb0KC7d7SR4JyMeEY4AsWf71KaM-uS6pw/exec"
 };
